ce593287b08bb7720dd197bb9a34e48cb77393a3
urgen sherpa <norbuurgen@gmail.com>
2017-02-20 16:42:42 +0545

    indentation corrected

--- terraform_example/autoscale.tf
+++ terraform_example/autoscale.tf
@@ -10,7 +10,6 @@ resource "aws_autoscaling_group" "ASG" {
     launch_configuration = "${aws_launch_configuration.demoLC.name}"
     load_balancers = ["${aws_elb.ELB-Terraform.name}"]
     health_check_type = "ELB"
-
     tag {
         key = "Name"
         value = "ec2-auto-scale-terraform"
@@ -22,7 +21,7 @@ resource "aws_launch_configuration" "demoLC" {
     name_prefix = "demoLC"
     image_id = "ami-892fe69f"
     instance_type = "t2.micro"
-     security_groups = ["sg-d1223aab"]
+    security_groups = ["sg-d1223aab"]
 
     lifecycle {
         create_before_destroy = true
@@ -37,7 +36,7 @@ resource "aws_launch_configuration" "demoLC" {
 resource "aws_elb" "ELB-Terraform" {
 name = "ELB-Terraform"
 security_groups = ["${aws_security_group.ELB-Terraform-SG.id}"]
-  availability_zones = [ "us-east-1b", "us-east-1c"]
+availability_zones = [ "us-east-1b", "us-east-1c"]
    
 health_check {
     healthy_threshold = 2
@@ -47,31 +46,31 @@ health_check {
     target = "HTTP:80/"
   }
 
-  listener {
+listener {
     instance_port = 80
     instance_protocol = "http"
     lb_port = 80
     lb_protocol = "http"
-  }
+    }
 }
 
 resource "aws_security_group" "ELB-Terraform-SG" {
- name = "terraform-SG"
-  ingress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
+    name = "terraform-SG"
+        ingress {
+            from_port = 0
+            to_port = 0
+            protocol = "-1"
+            cidr_blocks = ["0.0.0.0/0"]
+            }
 
-  egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
+        egress {
+           from_port = 0
+           to_port = 0
+           protocol = "-1"
+           cidr_blocks = ["0.0.0.0/0"]
+           }
 }
 
 output "elb_dns_name" {
-  value = "${aws_elb.ELB-Terraform.dns_name}"
+    value = "${aws_elb.ELB-Terraform.dns_name}"
 }
